c24e11473b177b015c9030f1c79b14921513fb62
victorborgesoliveira <vituze1@gmail.com>
2021-10-22 18:56:28 -0300

    add main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+const LinksSocialMedia = {
+  github: 'victorborgesoliveira',
+  youtube: 'jakelinygracielly',
+  facebook: 'victor.borges',
+  twitter: 'leaonoiado',
+  instagram: 'vitinrt7'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `http://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
